fe8784d8be38b466c302cb80d4c7662842b73986
snehal chavan <snehalwinner123@gmail.com>
2022-10-06 17:23:50 +0530

    hello

---
+++ model.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+app=Flask(__name__)
+with open("model.pickle","rb") as f:
+    model=pickle.load(f)
+with open("std.pickle","rb") as f:
+    std=pickle.load(f)
+@app.route('/',methods=["GET"])
+def fun1():
+    return render_template("index.html")
+@app.route("/rent",methods=["POST"])
+def pred():
+    gen=float(request.form['g'])
+    age=float(request.form['age'])
+    sal=float(request.form['sal'])
+   
+
+    data=np.array([[gen,age,sal]])
+    trans_data=std.transform(data)
+    result=model.predict(trans_data)
+    return render_template('index.html',prediction=result)
+if __name__=="__main__":
+    app.run(localhost="0.0.0.0",port=8080,debug=False)
